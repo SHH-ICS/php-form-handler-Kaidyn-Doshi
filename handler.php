33806aa0628e113bab41ebfdb766bc6809dd7962
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-03-01 16:02:16 +0000

    delete

--- handler.php
+++ handler.php
@@ -1,7 +1,27 @@
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css" />
+    <title>Handoff</title>
+  </head>
+
+  <body>
+    
+    <?php
+    $userName = "";
+    if ( isset( $_POST['userName'] ) ){
+      $userName = $_POST['userName'];
+    }
+    echo "<h1>Welcome ".$userName."!</h1>\n";
+    ?>
+        <?php
+    $email = "";
+    if ( isset( $_POST['email'] ) ){
+      $email = $_POST['email'];
+    }
+    echo "<h1>Your email is: ".$email."!</h1>\n";
+    ?>
+    
+  </body>
+  
+</html>
