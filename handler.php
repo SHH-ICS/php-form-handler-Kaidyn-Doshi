81111bef8b449812c6657041a8701d708031f339
Kaidyn <124370935+Kaidyn-Doshi@users.noreply.github.com>
2023-03-01 14:58:27 +0000

    commit

--- handler.php
+++ handler.php
@@ -1,16 +1,7 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['login'])) {
-        header('LOCATION:index.html'); die();
-    }
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-        <title>Correct Login page</title>
-  </head>
-    <body>
-        <?php echo "this page is only available to viridianknight7!!!"
-        ?>
-    </body> 
-</html>
